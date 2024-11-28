8b93bf34b2416fd2a0454f471042dcae6ee0485f
Hannes <h@nnes.dev>
2024-11-28 08:44:48 +0800

    Add flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    roc.url = "github:roc-lang/roc?rev=0295bb58da3de1dec84f63b5a1847ff32a3fd290";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "roc-lang.cachix.org-1:6lZeqLP9SadjmUbskJAvcdGR2T5ViR57pDVkxJQb8R4=";
+    extra-trusted-substituters = "https://roc-lang.cachix.org";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      roc,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem =
+        { inputs', pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "roc-html";
+            packages = [
+              inputs'.roc.packages.cli
+              pkgs.fd
+              pkgs.just
+              pkgs.nixfmt-rfc-style
+              pkgs.pre-commit
+            ];
+            shellHook = "pre-commit install --overwrite";
+          };
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+    };
+}
